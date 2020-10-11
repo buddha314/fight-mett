9ca424bb4d1af5f1a3b645b497f19b07b8523650
buddha314 <buddha_314@protonmail.com>
2020-10-10 23:15:05 -0700

    initial commit

---
+++ fightMett.js
@@ -0,0 +1,24 @@
+main()
+
+async function main() {
+  let fight = game.combats.combats[0]
+  let me = fight.combatant
+  let meWeapons = me.actor.data.items.filter((x) => ["weapon", "spell"].includes(x.type))
+  let enemies = fight.combatants.filter((x) => x.actor.data.type != me.actor.data.type)
+  let friends = fight.combatants.filter((x) => x.actor.data.type === me.actor.data.type)
+
+  console.log("\n" + me.name.toUpperCase() + " is fighting now, shut up.")
+  for (const e of enemies) {
+     let d =  Math.floor(Math.sqrt(Math.pow(me.token.x - e.token.x, 2) + Math.pow(me.token.y-e.token.y,2)) / 50) * 5
+     console.log('Enemy ' + e.name + ' is at (' + e.token.x + ',' + e.token.y + '): ' + d)
+     for (const w of meWeapons) {
+        if (w.data.range.value >= d) {
+           console.log("   I can hit with " + w.name)
+        }
+     }
+  }
+  for (const e of friends) {
+     console.log('Friend ' + e.name + ' is at (' + e.token.x + ',' + e.token.y + ')')
+  }
+
+}
