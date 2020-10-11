ed60f9440b51ff491fc4b53bf366a91858ce67db
buddha314 <buddha_314@protonmail.com>
2020-10-11 11:52:16 -0700

    adding Threat class

--- fightMett.js
+++ fightMett.js
@@ -1,17 +1,33 @@
+
+class Threat {
+  constructor() {
+    this.threatId;
+    this.threatName;
+    this.threatDistance;
+    this.threatWeapons;
+    this.threatIntimidation;
+    this.threatScore;
+  }
+};
+
+
 main()
 
 async function main() {
+
+
   let fight = game.combats.combats[0]
   let me = fight.combatant
   let meWeapons = me.actor.data.items.filter((x) => ["weapon", "spell"].includes(x.type))
-  let enemies = fight.combatants.filter((x) => x.actor.data.type != me.actor.data.type)
+  let enemies = identifyEnemies(me)
   let friends = fight.combatants.filter((x) => x.actor.data.type === me.actor.data.type)
 
   console.log("\n" + me.name.toUpperCase() + " is fighting now, shut up.")
   for (const e of enemies) {
      let d =  Math.floor(Math.sqrt(Math.pow(me.token.x - e.token.x, 2) + Math.pow(me.token.y-e.token.y,2)) / 50) * 5
      console.log('Enemy ' + e.name + ' is at (' + e.token.x + ',' + e.token.y + '): ' + d)
-     console.log('   threat level: ' + evaluateThreat(e, d))
+     let threat = evaluateThreat(e,d);
+     console.log('   threat level: ' + threat.threatScore)
      for (const w of meWeapons) {
         if (w.data.range.value >= d) {
            console.log("   I can hit with " + w.name)
@@ -24,11 +40,18 @@ async function main() {
 
 }
 
+function identifyEnemies(me) {
+  let enemies = fight.combatants.filter((x) => x.actor.data.type != me.actor.data.type)
+  return enemies
+}
+
 // Begin to evaluate the opponents threat level
 function evaluateThreat(opponent, distance) {
-  let threat = 0
-  let intimidation = opponent.actor.data.data.skills.itm.total
-  let threatWeapons = opponent.actor.data.items.filter((x) => ["weapon", "spell"].includes(x.type) &&  x.data.range.value >= distance)
+  let threat = new Threat();
+  threat.threatIntimidation = opponent.actor.data.data.skills.itm.total
+  threat.threatDistance = distance;
+  threat.threatScore = threat.threatIntimidation
+  threat.threatWeapons = opponent.actor.data.items.filter((x) => ["weapon", "spell"].includes(x.type) &&  x.data.range.value >= distance)
 
-  return intimidation + threat
+  return threat
 }
