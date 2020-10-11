6de1ef42f246b282f31734196b6a191f99abbaa8
buddha314 <buddha_314@protonmail.com>
2020-10-10 23:39:29 -0700

    starting threat assessment function

--- fightMett.js
+++ fightMett.js
@@ -11,6 +11,7 @@ async function main() {
   for (const e of enemies) {
      let d =  Math.floor(Math.sqrt(Math.pow(me.token.x - e.token.x, 2) + Math.pow(me.token.y-e.token.y,2)) / 50) * 5
      console.log('Enemy ' + e.name + ' is at (' + e.token.x + ',' + e.token.y + '): ' + d)
+     console.log('   threat level: ' + evaluateThreat(e, d))
      for (const w of meWeapons) {
         if (w.data.range.value >= d) {
            console.log("   I can hit with " + w.name)
@@ -21,4 +22,13 @@ async function main() {
      console.log('Friend ' + e.name + ' is at (' + e.token.x + ',' + e.token.y + ')')
   }
 
+}
+
+// Begin to evaluate the opponents threat level
+function evaluateThreat(opponent, distance) {
+  let threat = 0
+  let intimidation = opponent.actor.data.data.skills.itm.total
+  let threatWeapons = opponent.actor.data.items.filter((x) => ["weapon", "spell"].includes(x.type) &&  x.data.range.value >= distance)
+
+  return intimidation + threat
 }
