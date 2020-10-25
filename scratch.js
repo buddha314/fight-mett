13bd95dd4683f7bda1e2a83456fc2abf18e58656
buddha314 <buddha_314@protonmail.com>
2020-10-25 14:12:02 -0700

    fixing malformed json from features

--- scratch.js
+++ scratch.js
@@ -69,4 +69,26 @@ let me = game.combats.combats[0].combatant
 let you = game.combats.combats[0].combatants[2]
 chirp(fight, me, you)
 
- 
+
+
+let allFeatures = {"allFeatures":[
+  {
+    "name":"inMeleeWithMe",
+    "description":"Is the opponent actively hitting me with a melee weapon?",
+    "sophistication": -4,
+    "sophisticationAttribute":"me.combatant.actor.data.data.skills.inv.total",
+    "calc":"new Roll('d20').roll().total + you.combatant.actor.data.data.skills.itm.total"
+  },{
+    "name":"isIntimidating",
+    "description":"Does the opponent have a high intimidation skill",
+    "sophistication": -2,
+    "sophisticationAttribute":"me.combatant.actor.data.data.skills.wis.total",
+    "calc":"you.combatant.actor.data.data.skills.itm.total"
+  }, {
+    "name":"alliesSurrenduring",
+    "description":"Detect if your allies are still in the fight. This is determined by at least 50% still alive / engaged.  Not sure how to define this one yet, so just rolling a die.",
+    "sophistication": 1,
+    "sophisticationAttribute":"me.combatant.actor.data.data.skills.wis.total",
+    "calc":"new Roll('d20').roll().total"
+  }
+]}
--- scripts/main.js
+++ scripts/main.js
@@ -50,7 +50,25 @@ function findDistance(me, you) {
 
 function getAllFeatures() {
   let allFeatures = {"allFeatures":[
-    {"name":"inMelee"}
+    {
+      "name":"inMeleeWithMe",
+      "description":"Is the opponent actively hitting me with a melee weapon?",
+      "sophistication": -4,
+      "sophisticationAttribute":"me.combatant.actor.data.data.skills.inv.total",
+      "calc":"new Roll('d20').roll().total + you.combatant.actor.data.data.skills.itm.total"
+    },{
+      "name":"isIntimidating",
+      "description":"Does the opponent have a high intimidation skill",
+      "sophistication": -2,
+      "sophisticationAttribute":"me.combatant.actor.data.data.skills.wis.total",
+      "calc":"you.combatant.actor.data.data.skills.itm.total"
+    }, {
+      "name":"alliesSurrenduring",
+      "description":"Detect if your allies are still in the fight. This is determined by at least 50% still alive / engaged.  Not sure how to define this one yet, so just rolling a die.",
+      "sophistication": 1,
+      "sophisticationAttribute":"me.combatant.actor.data.data.skills.wis.total",
+      "calc":"new Roll('d20').roll().total"
+    }
   ]}
   return allFeatures
 }
