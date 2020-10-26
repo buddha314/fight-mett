62db4b039ddacc1ac20c00b9b8de588d4740628e
BLR-Robotics <doodliedoodliedolan@gmail.com>
2020-10-25 17:09:40 -0700

    Added basic quips

--- scripts/chirping.js
+++ scripts/chirping.js
@@ -1,5 +1,3 @@
-const allchirps = [`Hey ${you.combatant.actor.name} you shoulda stayed on the bench! Bender!`, 'insert witty comment here'];
-
 async function chirp(combat, me, you) {
   let x = new ChatBubbles();
   msg = await getChirp(combat, me, you)
@@ -9,8 +7,13 @@ async function chirp(combat, me, you) {
 }
 
 async function getChirp(combat, me, you) {
-const allchirps = [`Hey ${you.combatant.actor.name} you shoulda stayed on the bench! Bender!`, 'insert witty comment here'];
-    //msg = await `Hey ${you.combatant.actor.name} you shoulda stayed on the bench! Bender!`
+const allchirps = [`Hey ${you.combatant.actor.name} you shoulda stayed on the bench! Bender!`,
+  `${you.combatant.actor.name} is using a stick to hide their fragile masculinity. What a loser.`,
+  `Hey look everyone, its ${you.combatant.actor.name}! Get 'em!`,
+  `*points and laughs at ${you.combatant.actor.name}*`,
+  `Once Im done with you, Ill find your loved ones, ${you.combatant.actor.name}!`,
+  `I wont miss this time, ${you.combatant.actor.name}!`];
+//------------------------------------------------------------------------------
     msg = allchirps[Math.floor(Math.random() * allchirps.length)];
     return msg
 }
